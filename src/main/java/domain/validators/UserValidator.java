400cb9d3567f549370193f6f4f827203a350fe3e
Gabip19 <gabrielpaslaru01@gmail.com>
2022-11-22 00:29:44 +0200

    refactor UserValidator.validate

--- src/main/java/domain/validators/UserValidator.java
+++ src/main/java/domain/validators/UserValidator.java
@@ -22,11 +22,18 @@ public class UserValidator implements Validator<User> {
     @Override
     public void validate(User user) throws UserValidationException {
         String errors = "";
-        String firstName = user.getFirstName();
-        String lastName = user.getLastName();
-        String email = user.getEmail();
-        LocalDate date = user.getBirthdate();
 
+        errors = validateName(errors, user.getFirstName(), user.getLastName());
+        errors = validateEmail(errors, user.getEmail());
+        errors = validateDate(errors, user.getBirthdate());
+
+        // StringUtils.join(errors, "\n");
+        if (!(errors.equals(""))) {
+            throw new UserValidationException(errors);
+        }
+    }
+
+    private static String validateName(String errors, String firstName, String lastName) {
         if (lastName.length() < 2 || lastName.length() > 60 ||
             firstName.length() < 2 || firstName.length() > 60) {
             errors += "Name must have between 2 and 60 characters.\n";
@@ -37,16 +44,20 @@ public class UserValidator implements Validator<User> {
         if (!lastName.matches("^[a-zA-Z0-9-' ]+$") || !firstName.matches("^[a-zA-Z0-9-' ]+$")) {
             errors += "Unexpected symbol in name.\n";
         }
+        return errors;
+    }
+
+    private static String validateEmail(String errors, String email) {
         if (!email.matches("^[a-z]+[a-z0-9._-]+@[a-z]+[.][a-z]+$")) {
             errors += "Invalid email format.\n";
         }
+        return errors;
+    }
+
+    private static String validateDate(String errors, LocalDate date) {
         if (date.isBefore(LocalDate.of(1800, 1,1)) || date.isAfter(LocalDate.now().minusYears(3))) {
             errors += "User must be at least 3 years old and no older than 200 years.\n";
         }
-
-        // StringUtils.join(errors, "\n");
-        if (!(errors.equals(""))) {
-            throw new UserValidationException(errors);
-        }
+        return errors;
     }
 }
